68b2552d00fb20d50016839d2c1c3c36b8098e66
vireetha <dudalaviree69@gmail.com>
2024-12-24 05:25:20 +0530

    variable

---
+++ variables.py
@@ -0,0 +1,12 @@
+x = 10
+y = "Saisha"
+print(x)
+print(y)
+
+python = " High level language"
+print(python)
+
+#user Input
+name = input("enter your name: ")
+
+print("\n Hello", name, "\n Welcome to codingal")
