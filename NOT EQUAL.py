43dbbf94dcb6c721ab561e4115deed4a6a344721
vireetha <dudalaviree69@gmail.com>
2025-01-10 03:55:06 +0530

    meru

---
+++ NOT EQUAL.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+a = "Python"
+b = "Coding"
+
+if a != b:
+    print(a, 'and', b, 'are different.')
+a = 4
+b = 5
+if (a ==1) != (b==5):
+    print('Hello')
+a = int(input("Enter a number"))
+if a%2  != 0:
+    print(a,"is  not even number")
