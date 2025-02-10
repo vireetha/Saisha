fc2392cb46170888d770a2b225310ebd65e6b78e
vireetha <dudalaviree69@gmail.com>
2025-02-11 05:02:26 +0530

    7

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(x):
+    '''this is a recursive function to find the factorial of an integer'''
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+print("the factorial of 0: ", factorial(0))
+print("the factorial of 1: ", factorial(6))
+print("the factorial of 2 :", factorial(4))
+print("the factorial of 5: ", factorial(5))
+print("the factorial of 10: ", factorial(10))
